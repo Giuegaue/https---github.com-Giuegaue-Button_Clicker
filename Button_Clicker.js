c44f5f31f089f9d77a2b32d72df837207ac8c886
Zachary_Houston <ziphouston@gmail.com>
2021-10-19 16:49:35 -0700

    Origin

---
+++ Button_Clicker.js
@@ -0,0 +1,8 @@
+function like(id){
+    var element = document.querySelector("id").innerHTML;
+    var count = parseInt(element.innerHTML);
+    element.innerHTML = count +1;
+}
+function myFunction(){
+    document.getElementById('login-button').innerHTML = "Logout"
+}
